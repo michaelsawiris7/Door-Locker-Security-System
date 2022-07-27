ff65b1a25caf41402c614c92e3d90d184a96a259
Michael Samir Sawiris <68508914+michaelsawiris7@users.noreply.github.com>
2022-07-27 14:21:56 +0200

    Add files via upload

--- 1.UNIT2_c programming/1.c basics/Assignments/main.c
+++ 1.UNIT2_c programming/1.c basics/Assignments/main.c
@@ -5,17 +5,23 @@
  *      Author: Michael Samir
  */
 
-
-
-// Assignment 5
+// Assignment 6
 
 #include "stdio.h"
 int main()
 {
-	char x;
-	printf("Enter a character: ");
+	float a,b,c;
+	printf("Enter value of a: ");
 	fflush(stdout);
 	fflush(stdin);
-	scanf("%c",&x);
-	printf("ASCII value of %c = %i\n",x,x);
+	scanf("%f",&a);
+	printf("Enter value of b: ");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%f",&b);
+	c=a;a=b;b=c;
+	printf("After swapping, value of a = %.2f\n",a);
+	printf("After swapping, value of b = %.2f\n",b);
+
 }
+
